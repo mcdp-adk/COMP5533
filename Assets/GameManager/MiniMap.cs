f7abb182d47f9c9fa2225ad8ef3972a6db4de221
Crackpot1 <18846794509@163.com>
2025-04-10 21:06:50 +0800

    加入GameManager,Menu

---
+++ Assets/GameManager/MiniMap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public GameObject miniMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 按下 M 键时显示小地图
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            miniMap.SetActive(true);
+        }
+
+        // 松开 M 键时隐藏小地图
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            miniMap.SetActive(false);
+        }
+    }
+}
