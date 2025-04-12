66edf40bc932e7080c0e2bbd510247dbb76e6b92
Ponysesame <110590237+Ponysesame@users.noreply.github.com>
2025-04-12 18:02:41 +0800

    解耦了敌人生成和玩家生成的管理脚本

--- Assets/Scenes/EnemyAITest/charactercontrol.cs
+++ Assets/Scenes/EnemyAITest/charactercontrol.cs
@@ -5,19 +5,14 @@ using System.Collections;
 public class CharactersControl : MonoBehaviour
 {
     [Header("Prefabs")]
-    [SerializeField] private GameObject playerPrefab;
     [SerializeField] private GameObject enemyPrefab;
 
     [Header("Spawn Settings")]
-    [SerializeField] private Transform playerSpawnPoint;
     [SerializeField] private Transform[] enemySpawnPoints;
     [SerializeField] private int numberOfEnemies = 5;
 
     [Header("Patrol Points")]
     [SerializeField] private Transform[] patrolPoints;
-
-    [Header("Cinemachine")]
-    [SerializeField] private CinemachineVirtualCamera virtualCamera;
 
     public static CharactersControl Instance { get; private set; }
 
@@ -33,21 +28,8 @@ public class CharactersControl : MonoBehaviour
         }
     }
 
-    private void OnEnable()
-    {
-        PlayerController.OnPlayerDeath += PlayerDied;
-    }
-
-    private void OnDisable()
-    {
-        PlayerController.OnPlayerDeath -= PlayerDied;
-    }
-
     void Start()
     {
-        // 生成玩家
-        SpawnPlayer();
-
         // 生成AI敌人
         for (int i = 0; i < numberOfEnemies; i++)
         {
@@ -56,19 +38,6 @@ public class CharactersControl : MonoBehaviour
 
         // 开始检测敌人数量
         StartCoroutine(CheckEnemyCount());
-    }
-
-    // 生成玩家
-    private void SpawnPlayer()
-    {
-        GameObject player = Instantiate(playerPrefab, playerSpawnPoint.position, playerSpawnPoint.rotation);
-
-        // 设置Cinemachine虚拟摄像机的跟随目标
-        if (virtualCamera != null)
-        {
-            virtualCamera.Follow = player.transform;
-            virtualCamera.LookAt = player.transform;
-        }
     }
 
     // 生成敌人
@@ -84,19 +53,6 @@ public class CharactersControl : MonoBehaviour
         {
             enemyAI.patrolPoints = ShufflePatrolPoints(patrolPoints);
         }
-    }
-
-    // 玩家死亡处理方法
-    public void PlayerDied()
-    {
-        StartCoroutine(RespawnPlayer());
-    }
-
-    // 玩家重新生成协程
-    private IEnumerator RespawnPlayer()
-    {
-        yield return new WaitForSeconds(5f);
-        SpawnPlayer();
     }
 
     // 随机打乱巡逻点数组并返回一个新的数组实例
